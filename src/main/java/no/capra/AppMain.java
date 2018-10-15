9d77af362852d80e7131c6f25efbdfc0d81303b6
Håkon Nymo Matland <haakon@nymomatland.com>
2018-10-15 17:22:21 +0200

    Minimal pom with cantara-parent, testng and shaded jar

---
+++ src/main/java/no/capra/AppMain.java
@@ -0,0 +1,13 @@
+package no.capra;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/no/capra/AppMainTest.java
@@ -0,0 +1,21 @@
+package no.capra;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple AppMain.
+ */
+public class AppMainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
