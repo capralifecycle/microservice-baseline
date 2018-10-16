3fb1fc49b0a0d85d520e63dddec227f88e98250c
Henrik Steen <henrist@henrist.net>
2018-10-16 19:14:51 +0200

    Fix runapp issues
    - Ensure argument safety to date call and explicit UTC time
    - Fix return code check due to `set -e`

--- Docker/runapp.sh
+++ Docker/runapp.sh
@@ -4,7 +4,7 @@ set -e
 APP=app.jar
 JAVA_PARAMS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
 
-date +" --- RUNNING $(basename $0) %Y-%m-%d_%H:%M:%S --- "
+echo " --- RUNNING $(basename "$0") $(date -u "+%Y-%m-%d %H:%M:%S Z") --- "
 set -x
 
 # If AWS_PARAMETER_STORE_ENABLED equals "true"
@@ -12,8 +12,7 @@ set -x
 # The path is stripped to the output. eg /dev/myapp/db.password=1234 is added to the file as db.password=1234.
 if [ "$AWS_PARAMETER_STORE_ENABLED" = "true" ]; then
     mkdir -p "$(dirname "$AWS_PARAMETER_STORE_OUTPUTPATH")"
-    python GetPropertiesFromParameterStore.py $AWS_PARAMETER_STORE_PATH $AWS_PARAMETER_STORE_OUTPUTPATH
-    if [ $? -eq 0 ]; then
+    if python GetPropertiesFromParameterStore.py $AWS_PARAMETER_STORE_PATH $AWS_PARAMETER_STORE_OUTPUTPATH; then
         echo 'GetPropertiesFromParameterStore.py exited successfully!'
     else
         echo 'GetPropertiesFromParameterStore.py exited with error (non null exit code)'
